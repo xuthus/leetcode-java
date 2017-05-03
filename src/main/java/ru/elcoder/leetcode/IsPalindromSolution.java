f3deec3514b33d8be5b8ba46421a25ccb5be1645
sergey.yanzin <xuthus@yandex.ru>
2017-05-03 21:53:21 +0300

    removed duplicate Palindrome Number solutions, added description

--- src/main/java/ru/elcoder/leetcode/IsPalindromSolution.java
+++ src/main/java/ru/elcoder/leetcode/IsPalindromSolution.java
@@ -4,12 +4,15 @@ package ru.elcoder.leetcode;
  * Created by xuthus on 30.04.2017.
  */
 public class IsPalindromSolution {
-
+    /*
+    Determine whether an integer is a palindrome. Do this without extra space.
+    https://leetcode.com/problems/palindrome-number/
+    */
     public boolean isPalindrome(int x) {
         int t = x;
         if (x < 0)
             return false;
-        long y = 0;
+        long y = 0; //extra space
         while (x > 0) {
             y = y * 10 + x % 10;
             x = x / 10;
--- src/main/java/ru/elcoder/leetcode/IsPalindromSolutionV2.java
+++ src/main/java/ru/elcoder/leetcode/IsPalindromSolutionV2.java
@@ -4,13 +4,17 @@ package ru.elcoder.leetcode;
  * Created by xuthus on 30.04.2017.
  */
 public class IsPalindromSolutionV2 {
-
+    /*
+    Determine whether an integer is a palindrome. Do this without extra space.
+    https://leetcode.com/problems/palindrome-number/
+    */
+    // faster 2x and 2x lesser extra space
     public boolean isPalindrome(int x) {
         if (x == 0)
             return true;
         if (x < 0 || x % 10 == 0)
             return false;
-        int y = 0;
+        int y = 0;  //extra space
         while (x > y) {
             y = y * 10 + x % 10;
             x = x / 10;
