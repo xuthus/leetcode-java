1ada7cc4dde7ba7c1fed49d584ba6ce79dca17ed
sergey.yanzin <xuthus@yandex.ru>
2017-05-10 19:57:28 +0300

    ListNode refactorings

--- src/main/java/ru/elcoder/leetcode/models/ListNode.java
+++ src/main/java/ru/elcoder/leetcode/models/ListNode.java
@@ -14,4 +14,41 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    public static ListNode fromArray(int... items) {
+        ListNode head = null;
+        ListNode parent = null;
+        for (int item : items) {
+            ListNode node = new ListNode(item);
+            if (parent == null)
+                parent = head = node;
+            else {
+                parent.next = node;
+                parent = node;
+            }
+        }
+        return head;
+    }
+
+    public int[] toArray() {
+        int length = 0;
+        ListNode node = this;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        int[] ints = new int[length];
+        length = 0;
+        node = this;
+        while (node != null) {
+            ints[length++] = node.val;
+            node = node.next;
+        }
+        return ints;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder("{val = ").append(val).append(", next = ").append(next == null ? "null" : next.val).append("}").toString();
+    }
 }
--- src/test/java/ru/elcoder/leetcode/ReverseLinkedListSolutionTest.java
+++ src/test/java/ru/elcoder/leetcode/ReverseLinkedListSolutionTest.java
@@ -3,9 +3,8 @@ package ru.elcoder.leetcode;
 import org.junit.Test;
 import ru.elcoder.leetcode.models.ListNode;
 
-import java.util.ArrayList;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by xuthus on 01.05.2017.
@@ -16,40 +15,10 @@ public class ReverseLinkedListSolutionTest {
     public void testReverseList() throws Exception {
         ReverseLinkedListSolution solution = new ReverseLinkedListSolution();
 
-        assertArrayEquals(new int[]{1, 2, 3, 4}, listToArray(solution.reverseList(newLinkedList(4, 3, 2, 1))));
-        assertArrayEquals(new int[]{1}, listToArray(solution.reverseList(newLinkedList(1))));
+        assertArrayEquals(new int[]{1, 2, 3, 4}, solution.reverseList(ListNode.fromArray(4, 3, 2, 1)).toArray());
+        assertArrayEquals(new int[]{1}, solution.reverseList(ListNode.fromArray(1)).toArray());
         assertEquals(null, solution.reverseList(null));
     }
 
-    private int[] listToArray(ListNode listNode) {
-        int length = 0;
-        ListNode node = listNode;
-        while (node != null) {
-            length++;
-            node = node.next;
-        }
-        int[] ints = new int[length];
-        length = 0;
-        node = listNode;
-        while (node != null) {
-            ints[length++] = node.val;
-            node = node.next;
-        }
-        return ints;
-    }
 
-    private ListNode newLinkedList(int... items) {
-        ListNode head = null;
-        ListNode parent = null;
-        for (int item : items) {
-            ListNode node = new ListNode(item);
-            if (parent == null)
-                parent = head = node;
-            else {
-                parent.next = node;
-                parent = node;
-            }
-        }
-        return head;
-    }
 }
