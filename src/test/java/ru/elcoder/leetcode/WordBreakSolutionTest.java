2a2f0a0364bf138439a39e5dd6725ce6d5da0da1
sergey <xuthus@yandex.ru>
2017-07-12 18:51:29 +0300

    typo in WordBreakSolutionTest

--- src/test/java/ru/elcoder/leetcode/WordBreakSolutionTest.java
+++ src/test/java/ru/elcoder/leetcode/WordBreakSolutionTest.java
@@ -13,61 +13,34 @@ import static org.junit.Assert.assertTrue;
  */
 public class WordBreakSolutionTest {
 
-    @Test
-    public void wordBreak() throws Exception {
-        WordBreakSolution solution = new WordBreakSolution();
-
-        assertTrue(solution.wordBreak("bccdbacdbdacddabbaaaadababadad", list("cbc", "bcda", "adb", "ddca", "bad", "bbb",
-                "dad", "dac", "ba", "aa", "bd", "abab", "bb", "dbda", "cb", "caccc", "d", "dd", "aadb", "cc", "b", "bcc",
-                "bcd", "cd", "cbca", "bbd", "ddd", "dabb", "ab", "acd", "a", "bbcc", "cdcbd", "cada", "dbca", "ac",
-                "abacd", "cba", "cdb", "dbac", "aada", "cdcda", "cdc", "dbc", "dbcb", "bdb", "ddbdd", "cadaa", "ddbc",
-                "babb")));
-
-        assertFalse(solution.wordBreak("leetcodeleets", list("leet", "code")));
-        assertTrue(solution.wordBreak("bombeebom", list("bom", "bee")));
-        assertTrue(solution.wordBreak("hastalavista", list("vista", "hasta", "la")));
-        assertFalse(solution.wordBreak("z", list("vista", "hasta", "la")));
-        assertFalse(solution.wordBreak("z", null));
-        assertFalse(solution.wordBreak(null, list("vista", "hasta", "la")));
-        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b")));
-        assertFalse(solution.wordBreak("abc", list("d", "c", "a", "be")));
-        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b", "")));
-        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b", "b")));
-        assertTrue(solution.wordBreak("abcd", list("a", "abc", "b", "cd")));
-        assertTrue(solution.wordBreak("abcd", list("a", "abc", "b", "cd", "ab")));
-
-        assertFalse(solution.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
-                list("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa")));
-    }
-
-    @Test
-    public void wordBreak() throws Exception {
-        WordBreakSolution solution = new WordBreakSolution();
-
-        assertTrue(solution.wordBreak("bccdbacdbdacddabbaaaadababadad", list("cbc", "bcda", "adb", "ddca", "bad", "bbb",
-                "dad", "dac", "ba", "aa", "bd", "abab", "bb", "dbda", "cb", "caccc", "d", "dd", "aadb", "cc", "b", "bcc",
-                "bcd", "cd", "cbca", "bbd", "ddd", "dabb", "ab", "acd", "a", "bbcc", "cdcbd", "cada", "dbca", "ac",
-                "abacd", "cba", "cdb", "dbac", "aada", "cdcda", "cdc", "dbc", "dbcb", "bdb", "ddbdd", "cadaa", "ddbc",
-                "babb")));
-
-        assertFalse(solution.wordBreak("leetcodeleets", list("leet", "code")));
-        assertTrue(solution.wordBreak("bombeebom", list("bom", "bee")));
-        assertTrue(solution.wordBreak("hastalavista", list("vista", "hasta", "la")));
-        assertFalse(solution.wordBreak("z", list("vista", "hasta", "la")));
-        assertFalse(solution.wordBreak("z", null));
-        assertFalse(solution.wordBreak(null, list("vista", "hasta", "la")));
-        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b")));
-        assertFalse(solution.wordBreak("abc", list("d", "c", "a", "be")));
-        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b", "")));
-        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b", "b")));
-        assertTrue(solution.wordBreak("abcd", list("a", "abc", "b", "cd")));
-        assertTrue(solution.wordBreak("abcd", list("a", "abc", "b", "cd", "ab")));
-
-        assertFalse(solution.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
-                list("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa")));
-    }
-
     private static List<String> list(String... arrayOfStrings) {
         return Arrays.asList(arrayOfStrings);
+    }
+
+    @Test
+    public void wordBreak() throws Exception {
+        WordBreakSolution solution = new WordBreakSolution();
+
+        assertTrue(solution.wordBreak("bccdbacdbdacddabbaaaadababadad", list("cbc", "bcda", "adb", "ddca", "bad", "bbb",
+                "dad", "dac", "ba", "aa", "bd", "abab", "bb", "dbda", "cb", "caccc", "d", "dd", "aadb", "cc", "b", "bcc",
+                "bcd", "cd", "cbca", "bbd", "ddd", "dabb", "ab", "acd", "a", "bbcc", "cdcbd", "cada", "dbca", "ac",
+                "abacd", "cba", "cdb", "dbac", "aada", "cdcda", "cdc", "dbc", "dbcb", "bdb", "ddbdd", "cadaa", "ddbc",
+                "babb")));
+
+        assertFalse(solution.wordBreak("leetcodeleets", list("leet", "code")));
+        assertTrue(solution.wordBreak("bombeebom", list("bom", "bee")));
+        assertTrue(solution.wordBreak("hastalavista", list("vista", "hasta", "la")));
+        assertFalse(solution.wordBreak("z", list("vista", "hasta", "la")));
+        assertFalse(solution.wordBreak("z", null));
+        assertFalse(solution.wordBreak(null, list("vista", "hasta", "la")));
+        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b")));
+        assertFalse(solution.wordBreak("abc", list("d", "c", "a", "be")));
+        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b", "")));
+        assertTrue(solution.wordBreak("abc", list("d", "c", "a", "b", "b")));
+        assertTrue(solution.wordBreak("abcd", list("a", "abc", "b", "cd")));
+        assertTrue(solution.wordBreak("abcd", list("a", "abc", "b", "cd", "ab")));
+
+        assertFalse(solution.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+                list("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa")));
     }
 }
