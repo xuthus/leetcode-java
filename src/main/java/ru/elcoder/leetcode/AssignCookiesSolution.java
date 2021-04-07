8bcaa7a639e7e62aae7383671615a543fc4105f7
Sergey Yanzin <xuthus@yandex.ru>
2021-04-07 14:18:24 +0400

    Assign Cookies Solution

--- src/main/java/ru/elcoder/leetcode/AssignCookiesSolution.java
+++ src/main/java/ru/elcoder/leetcode/AssignCookiesSolution.java
@@ -3,13 +3,15 @@ package ru.elcoder.leetcode;
 import ru.elcoder.leetcode.models.DifficultyLevel;
 import ru.elcoder.leetcode.models.Leetcode;
 
+import java.util.Arrays;
+
 /**
- * Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at
- * most one cookie.
+ * <p>Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at
+ * most one cookie.</p>
  *
- * Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with;
+ * <p>Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with;
  * and each cookie j has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i
- * will be content. Your goal is to maximize the number of your content children and output the maximum number.
+ * will be content. Your goal is to maximize the number of your content children and output the maximum number.</p>
  */
 @Leetcode(
         difficulty = DifficultyLevel.Easy,
@@ -19,6 +21,19 @@ import ru.elcoder.leetcode.models.Leetcode;
 )
 public class AssignCookiesSolution {
     public int findContentChildren(int[] g, int[] s) {
-        return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int start = 0;
+        int count = 0;
+        for (int greed : g) {
+            for (int i = start; i < s.length; i++) {
+                if (greed <= s[i]) {
+                    start = i + 1;
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
     }
 }
--- src/test/java/ru/elcoder/leetcode/AssignCookiesSolutionTest.java
+++ src/test/java/ru/elcoder/leetcode/AssignCookiesSolutionTest.java
@@ -2,11 +2,27 @@ package ru.elcoder.leetcode;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static ru.elcoder.leetcode.utils.Utils.arrayOf;
 
 public class AssignCookiesSolutionTest {
 
     @Test
-    public void findContentChildren() {
+    public void findContentChildren1() {
+        final AssignCookiesSolution solution = new AssignCookiesSolution();
+        assertEquals(1, solution.findContentChildren(arrayOf(1, 2, 3), arrayOf(1, 1)));
+    }
+
+    @Test
+    public void findContentChildren2() {
+        final AssignCookiesSolution solution = new AssignCookiesSolution();
+        assertEquals(2, solution.findContentChildren(arrayOf(1, 2), arrayOf(1, 2, 3)));
+    }
+
+    @Test
+    public void findContentChildrenBorderCases() {
+        final AssignCookiesSolution solution = new AssignCookiesSolution();
+        assertEquals(0, solution.findContentChildren(new int[0], arrayOf(1, 2, 3)));
+        assertEquals(0, solution.findContentChildren(arrayOf(1, 2, 3), new int[0]));
     }
 }
