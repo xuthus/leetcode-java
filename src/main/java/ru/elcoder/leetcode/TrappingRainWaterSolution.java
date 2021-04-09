a6da2e417f97f236decf84d2c277be2884a21431
Sergey Yanzin <xuthus@yandex.ru>
2021-04-09 16:43:28 +0400

    WIP: Trapping Rain Water solution

---
+++ src/main/java/ru/elcoder/leetcode/TrappingRainWaterSolution.java
@@ -0,0 +1,16 @@
+package ru.elcoder.leetcode;
+
+import ru.elcoder.leetcode.models.DifficultyLevel;
+import ru.elcoder.leetcode.models.Leetcode;
+
+@Leetcode(
+        difficulty = DifficultyLevel.Hard,
+        number = 42,
+        title = "Trapping Rain Water",
+        url = "https://leetcode.com/problems/trapping-rain-water/"
+)
+public class TrappingRainWaterSolution {
+    public int trap(int[] height) {
+        return 0;
+    }
+}
---
+++ src/test/java/ru/elcoder/leetcode/TrappingRainWaterSolutionTest.java
@@ -0,0 +1,21 @@
+package ru.elcoder.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static ru.elcoder.leetcode.utils.Utils.arrayOf;
+
+public class TrappingRainWaterSolutionTest {
+
+    @Test
+    public void trap1() {
+        final TrappingRainWaterSolution solution = new TrappingRainWaterSolution();
+        assertEquals(6, solution.trap(arrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)));
+    }
+
+    @Test
+    public void trap2() {
+        final TrappingRainWaterSolution solution = new TrappingRainWaterSolution();
+        assertEquals(9, solution.trap(arrayOf(4, 2, 0, 3, 2, 5)));
+    }
+}
