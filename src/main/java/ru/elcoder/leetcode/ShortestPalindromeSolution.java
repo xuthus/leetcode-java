202aea753cb57c2a70170e4a0ab99569533e5067
Sergey Yanzin <xuthus@yandex.ru>
2021-04-15 12:11:37 +0400

    Shortest Palindrome solution

--- src/main/java/ru/elcoder/leetcode/ShortestPalindromeSolution.java
+++ src/main/java/ru/elcoder/leetcode/ShortestPalindromeSolution.java
@@ -11,7 +11,46 @@ import ru.elcoder.leetcode.models.Leetcode;
 )
 public class ShortestPalindromeSolution {
     public String shortestPalindrome(String s) {
-        // find minimal radius using binary search
-        return s;
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        final char c0 = s.charAt(0);
+        final int maxIndex = s.length() - 1;
+        final Boolean[] memo = new Boolean[s.length()];
+        for (int i = maxIndex; i >= 0; i--) {
+            if (s.charAt(i) == c0 && isPalindrome(s, 0, i, memo)) {
+                return reversed(s, i + 1, maxIndex) + s;
+            }
+        }
+        return s; // never be reached
+    }
+
+    public String reversed(String s, int p1, int p2) {
+        if (p2 >= s.length()) {
+            return "";
+        }
+        final StringBuilder sb = new StringBuilder(p2 - p1 + 1);
+        for (int i = p2; i >= p1; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public boolean isPalindrome(String s, int p1, int p2, Boolean[] memo) {
+        if (memo[p2] != null) {
+            return memo[p2];
+        }
+        int key = p2;
+        boolean result = true;
+        while (p1 < p2) {
+            if (s.charAt(p1) != s.charAt(p2)) {
+                result = false;
+                break;
+            }
+            p1++;
+            p2--;
+        }
+        memo[key] = result;
+        return result;
     }
 }
--- src/test/java/ru/elcoder/leetcode/ShortestPalindromeSolutionTest.java
+++ src/test/java/ru/elcoder/leetcode/ShortestPalindromeSolutionTest.java
@@ -2,7 +2,9 @@ package ru.elcoder.leetcode;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ShortestPalindromeSolutionTest {
 
@@ -46,5 +48,31 @@ public class ShortestPalindromeSolutionTest {
     public void shortestPalindrome7() {
         final ShortestPalindromeSolution solution = new ShortestPalindromeSolution();
         assertEquals("cbabc", solution.shortestPalindrome("babc"));
+    }
+
+    @Test
+    public void shortestPalindrome8() {
+        final ShortestPalindromeSolution solution = new ShortestPalindromeSolution();
+        assertEquals("ababbabbbabbaba", solution.shortestPalindrome("babbbabbaba"));
+    }
+
+    @Test
+    public void isPalindrome() {
+        final ShortestPalindromeSolution solution = new ShortestPalindromeSolution();
+        assertTrue(solution.isPalindrome("babbbabbaba", 0, 6, new Boolean[100]));
+        assertTrue(solution.isPalindrome("aba", 0, 2, new Boolean[100]));
+        assertTrue(solution.isPalindrome("baba", 1, 3, new Boolean[100]));
+        assertTrue(solution.isPalindrome("abba", 0, 3, new Boolean[100]));
+        assertTrue(solution.isPalindrome("abbac", 0, 3, new Boolean[100]));
+        assertTrue(solution.isPalindrome("abbac", 1, 2, new Boolean[100]));
+        assertFalse(solution.isPalindrome("abbac", 1, 3, new Boolean[100]));
+        assertFalse(solution.isPalindrome("abbac", 0, 4, new Boolean[100]));
+    }
+
+    @Test
+    public void reversed() {
+        final ShortestPalindromeSolution solution = new ShortestPalindromeSolution();
+        assertEquals("abc", solution.reversed("111cba", 3, 5));
+        assertEquals("", solution.reversed("111cba", 6, 5));
     }
 }
