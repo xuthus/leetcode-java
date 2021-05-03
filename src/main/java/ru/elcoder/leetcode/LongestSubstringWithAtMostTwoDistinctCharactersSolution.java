fbcdf5fe0a1e3ba5563b334360ffa65b0b81386d
Sergey Yanzin <yanzinsg@gmail.com>
2021-05-03 22:06:33 +0400

    WIP: Longest Substring with At Most Two Distinct Characters solution

---
+++ src/main/java/ru/elcoder/leetcode/LongestSubstringWithAtMostTwoDistinctCharactersSolution.java
@@ -0,0 +1,17 @@
+package ru.elcoder.leetcode;
+
+import ru.elcoder.leetcode.models.DifficultyLevel;
+import ru.elcoder.leetcode.models.Leetcode;
+
+@Leetcode(
+        difficulty = DifficultyLevel.Medium,
+        number = 159,
+        title = "Longest Substring with At Most Two Distinct Characters",
+        url = "https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/"
+)
+public class LongestSubstringWithAtMostTwoDistinctCharactersSolution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        // todo: implement
+        return 0;
+    }
+}
