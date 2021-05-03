c89f497d7f550d54ee18d97f8df3db8d6a426cc5
Sergey Yanzin <yanzinsg@gmail.com>
2021-05-03 22:09:21 +0400

    WIP: Partition Labels solution

---
+++ src/main/java/ru/elcoder/leetcode/PartitionLabelsSolution.java
@@ -0,0 +1,19 @@
+package ru.elcoder.leetcode;
+
+import ru.elcoder.leetcode.models.DifficultyLevel;
+import ru.elcoder.leetcode.models.Leetcode;
+
+import java.util.List;
+
+@Leetcode(
+        difficulty = DifficultyLevel.Medium,
+        number = 763,
+        title = "Partition Labels",
+        url = "https://leetcode.com/problems/partition-labels/"
+)
+public class PartitionLabelsSolution {
+    public List<Integer> partitionLabels(String S) {
+        // todo: implement
+        return null;
+    }
+}
---
+++ src/test/java/ru/elcoder/leetcode/PartitionLabelsSolutionTest.java
@@ -0,0 +1,12 @@
+package ru.elcoder.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PartitionLabelsSolutionTest {
+
+    @Test
+    public void partitionLabels() {
+    }
+}
