ff45054f7ba9a04d932bf4cdc50f07d7e2761c6f
Sergey Yanzin <yanzinsg@gmail.com>
2021-05-03 22:03:01 +0400

    WIP: Missing Ranges solution

---
+++ src/main/java/ru/elcoder/leetcode/MissingRangesSolution.java
@@ -0,0 +1,19 @@
+package ru.elcoder.leetcode;
+
+import ru.elcoder.leetcode.models.DifficultyLevel;
+import ru.elcoder.leetcode.models.Leetcode;
+
+import java.util.List;
+
+@Leetcode(
+        difficulty = DifficultyLevel.Easy,
+        number = 163,
+        title = "Missing Ranges",
+        url = "https://leetcode.com/problems/missing-ranges/"
+)
+public class MissingRangesSolution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        // todo: implement
+        return null;
+    }
+}
