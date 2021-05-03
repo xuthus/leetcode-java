89dbf85846ed5581cd9d782bc6ebbfe6b071b662
Sergey Yanzin <yanzinsg@gmail.com>
2021-05-03 21:59:28 +0400

    WIP: Backspace String Compare solution

---
+++ src/main/java/ru/elcoder/leetcode/BackspaceStringCompareSolution.java
@@ -0,0 +1,16 @@
+package ru.elcoder.leetcode;
+
+import ru.elcoder.leetcode.models.DifficultyLevel;
+import ru.elcoder.leetcode.models.Leetcode;
+
+@Leetcode(
+        difficulty = DifficultyLevel.Easy,
+        number = 844,
+        title = "Backspace String Compare",
+        url = "https://leetcode.com/problems/backspace-string-compare/"
+)
+public class BackspaceStringCompareSolution {
+    public boolean backspaceCompare(String s, String t) {
+        return false;
+    }
+}
