07fba697ce943455c92bfdf83c0be8b757da8cfd
Sergey Yanzin <xuthus@yandex.ru>
2021-05-04 10:33:33 +0400

    Partition Labels solution

--- src/main/java/ru/elcoder/leetcode/PartitionLabelsSolution.java
+++ src/main/java/ru/elcoder/leetcode/PartitionLabelsSolution.java
@@ -3,6 +3,7 @@ package ru.elcoder.leetcode;
 import ru.elcoder.leetcode.models.DifficultyLevel;
 import ru.elcoder.leetcode.models.Leetcode;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Leetcode(
@@ -13,7 +14,19 @@ import java.util.List;
 )
 public class PartitionLabelsSolution {
     public List<Integer> partitionLabels(String S) {
-        // todo: implement
-        return null;
+        List<Integer> result = new LinkedList<>();
+        int[] right = new int[255];
+        for (int i = S.length() - 1; i >= 0; i--)
+            if (right[S.charAt(i)] == 0)
+                right[S.charAt(i)] = i;
+        int start = 0, end = -1;
+        for (int i = 0; i < S.length(); i++) {
+            end = Math.max(end, right[S.charAt(i)]);
+            if (i >= end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/ru/elcoder/leetcode/PartitionLabelsSolutionTest.java
+++ src/test/java/ru/elcoder/leetcode/PartitionLabelsSolutionTest.java
@@ -1,12 +1,19 @@
 package ru.elcoder.leetcode;
 
 import org.junit.Test;
+import ru.elcoder.leetcode.utils.Utils;
 
 import static org.junit.Assert.*;
+import static ru.elcoder.leetcode.utils.Utils.arrayOf;
 
 public class PartitionLabelsSolutionTest {
 
     @Test
     public void partitionLabels() {
+        final PartitionLabelsSolution solution = new PartitionLabelsSolution();
+        assertArrayEquals(arrayOf(1), Utils.toIntArray(solution.partitionLabels("a")));
+        assertArrayEquals(arrayOf(1, 1), Utils.toIntArray(solution.partitionLabels("ab")));
+        assertArrayEquals(arrayOf(3), Utils.toIntArray(solution.partitionLabels("aba")));
+        assertArrayEquals(arrayOf(3, 3), Utils.toIntArray(solution.partitionLabels("abacec")));
     }
 }
