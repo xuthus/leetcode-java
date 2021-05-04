6c9af3d928bf90f3b9540b05935e9f583cdffe7f
Sergey Yanzin <xuthus@yandex.ru>
2021-05-04 09:57:45 +0400

    Missing Ranges solution

--- src/main/java/ru/elcoder/leetcode/MissingRangesSolution.java
+++ src/main/java/ru/elcoder/leetcode/MissingRangesSolution.java
@@ -3,6 +3,7 @@ package ru.elcoder.leetcode;
 import ru.elcoder.leetcode.models.DifficultyLevel;
 import ru.elcoder.leetcode.models.Leetcode;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Leetcode(
@@ -13,7 +14,24 @@ import java.util.List;
 )
 public class MissingRangesSolution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-        // todo: implement
-        return null;
+        List<String> result = new LinkedList<>();
+        if (nums.length == 0) {
+            if (lower <= upper) addRange(result, lower, upper);
+        } else {
+            if (lower < nums[0]) addRange(result, lower, nums[0] - 1);
+            for (int i = 0; i < nums.length - 1; i++)
+                if (nums[i] + 1 < nums[i + 1]) addRange(result, nums[i] + 1, nums[i + 1] - 1);
+            if (nums[nums.length - 1] < upper) addRange(result, nums[nums.length - 1] + 1, upper);
+        }
+        return result;
+    }
+
+    private void addRange(List<String> result, int lower, int upper) {
+        if (lower > upper)
+            return;
+        if (lower == upper)
+            result.add("" + lower);
+        else
+            result.add(Integer.toString(lower) + "->" + Integer.toString(upper));
     }
 }
--- src/test/java/ru/elcoder/leetcode/MissingRangesSolutionTest.java
+++ src/test/java/ru/elcoder/leetcode/MissingRangesSolutionTest.java
@@ -2,11 +2,24 @@ package ru.elcoder.leetcode;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static ru.elcoder.leetcode.utils.Utils.arrayOf;
 
 public class MissingRangesSolutionTest {
 
     @Test
     public void findMissingRanges() {
+        final MissingRangesSolution solution = new MissingRangesSolution();
+        assertEquals("1", String.join(",", solution.findMissingRanges(arrayOf(0), 0, 1)));
+        assertEquals("1->10", String.join(",",
+                solution.findMissingRanges(arrayOf(0), 0, 10)));
+        assertEquals("1->9", String.join(",",
+                solution.findMissingRanges(arrayOf(0, 10), 0, 10)));
+        assertEquals("1->8,10", String.join(",",
+                solution.findMissingRanges(arrayOf(0, 9), 0, 10)));
+        assertEquals("1", String.join(",",
+                solution.findMissingRanges(arrayOf(0, 2), 0, 2)));
+        assertEquals("1->4", String.join(",",
+                solution.findMissingRanges(arrayOf(0, 5), 0, 5)));
     }
 }
