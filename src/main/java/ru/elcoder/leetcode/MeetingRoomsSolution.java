9c2d7695ce6f3d93f8302d1a204ccb6e111ee175
Sergey Yanzin <xuthus@yandex.ru>
2021-05-05 07:44:52 +0400

    Meeting Rooms solution

--- src/main/java/ru/elcoder/leetcode/MeetingRoomsSolution.java
+++ src/main/java/ru/elcoder/leetcode/MeetingRoomsSolution.java
@@ -3,6 +3,9 @@ package ru.elcoder.leetcode;
 import ru.elcoder.leetcode.models.DifficultyLevel;
 import ru.elcoder.leetcode.models.Leetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 @Leetcode(
         difficulty = DifficultyLevel.Easy,
         number = 252,
@@ -11,6 +14,12 @@ import ru.elcoder.leetcode.models.Leetcode;
 )
 public class MeetingRoomsSolution {
     public boolean canAttendMeetings(int[][] intervals) {
-        return false; // TODO: 5/4/2021 implement
+        if (intervals.length < 2)
+            return true;
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+        for (int i = 0; i < intervals.length - 1; i++)
+            if (intervals[i][1] > intervals[i + 1][0])
+                return false;
+        return true;
     }
 }
--- src/test/java/ru/elcoder/leetcode/MeetingRoomsSolutionTest.java
+++ src/test/java/ru/elcoder/leetcode/MeetingRoomsSolutionTest.java
@@ -3,11 +3,16 @@ package ru.elcoder.leetcode;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static ru.elcoder.leetcode.utils.Utils.arrayOf;
 
 public class MeetingRoomsSolutionTest {
 
     @Test
     public void canAttendMeetings() {
-
+        final MeetingRoomsSolution solution = new MeetingRoomsSolution();
+        assertTrue(solution.canAttendMeetings(new int[][] {
+                arrayOf(10, 15),
+                arrayOf(2, 7)
+        }));
     }
 }
