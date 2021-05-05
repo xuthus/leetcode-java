9de08cb1080b5df7f3be8e1aa51fb34633717395
Sergey Yanzin <xuthus@yandex.ru>
2021-05-05 09:37:03 +0400

    Number Of Islands solution

--- src/main/java/ru/elcoder/leetcode/NumberOfIslandsSolution.java
+++ src/main/java/ru/elcoder/leetcode/NumberOfIslandsSolution.java
@@ -11,6 +11,28 @@ import ru.elcoder.leetcode.models.Leetcode;
 )
 public class NumberOfIslandsSolution {
     public int numIslands(char[][] grid) {
-        return -1; // TODO: 5/4/2021 implement
+        int result = 0;
+        if (grid.length > 0 && grid[0].length > 0)
+            for (int r = 0; r < grid.length; r++)
+                for (int c = 0; c < grid[0].length; c++)
+                    if (grid[r][c] == '1') {
+                        result++;
+                        grid[r][c] = '2';
+                        dfs(grid, r - 1, c);
+                        dfs(grid, r + 1, c);
+                        dfs(grid, r, c + 1);
+                        dfs(grid, r, c - 1);
+                    }
+        return result;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] != '1')
+            return;
+        grid[r][c] = '2';
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
     }
 }
--- src/test/java/ru/elcoder/leetcode/NumberOfIslandsSolutionTest.java
+++ src/test/java/ru/elcoder/leetcode/NumberOfIslandsSolutionTest.java
@@ -8,5 +8,11 @@ public class NumberOfIslandsSolutionTest {
 
     @Test
     public void numIslands() {
+        final NumberOfIslandsSolution solution = new NumberOfIslandsSolution();
+        assertEquals(1, solution.numIslands(new char[][]{
+                new char[] {'1', '1', '1'},
+                new char[] {'0', '1', '0'},
+                new char[] {'1', '1', '1'}
+        }));
     }
 }
