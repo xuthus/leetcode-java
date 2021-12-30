214212726b65396f066de6bcbaf025a2f386208f
Sergey Yanzin <xuthus@yandex.ru>
2021-12-30 18:15:14 +0400

    Stepik: Points and Lines

---
+++ src/main/java/ru/elcoder/stepik/algorithms/quicksort/PointsSolution.java
@@ -0,0 +1,108 @@
+package ru.elcoder.stepik.algorithms.quicksort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class PointsSolution {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int m = scanner.nextInt();
+        final Area[] areasFrom = new Area[n];
+        for (int i = 0; i < n; i++) {
+            areasFrom[i] = new Area(scanner.nextInt(), scanner.nextInt());
+        }
+        final Area[] areasTo = Arrays.copyOf(areasFrom, areasFrom.length);
+        Arrays.sort(areasFrom, Comparator.comparingInt(o -> o.from));
+        Arrays.sort(areasTo, Comparator.comparingInt(o -> o.to));
+
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int point = scanner.nextInt();
+            builder.append(owningAreasCount(point, areasFrom, areasTo)).append(' ');
+        }
+
+        System.out.println(builder.substring(0, builder.length() - 1));
+    }
+
+    public static int owningAreasCount(int point, Area[] areasFrom, Area[] areasTo) {
+        int finished = searchFinishedNaive(point, areasTo);
+        int started = searchStartedQuick(point, areasFrom);
+        if (finished == -1 || started == -1) {
+            return 0;
+        }
+        return started - finished;
+    }
+
+    public static int searchStartedNaive(int point, Area[] areas) {
+        // naive scan
+        for (int i = 0; i < areas.length; i++) {
+            if (areas[i].from > point) {
+                return i;
+            }
+        }
+        return areas.length;
+    }
+
+    public static int searchStartedQuick(int point, Area[] areas) {
+        int l = 0;
+        int r = areas.length - 1;
+        if (areas[l].from > point) {
+            return 0;
+        }
+        if (areas[r].from < point) {
+            return r + 1;
+        }
+        while (l < r) {
+            int m = (l + r) >> 1;
+            if (areas[m].from > point) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return (areas[l].from > point) ? l : l + 1;
+    }
+
+    public static int searchFinishedNaive(int point, Area[] areas) {
+        // naive scan
+        for (int i = areas.length - 1; i >= 0; i--) {
+            if (areas[i].to < point) {
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+
+    public static int searchFinishedQuick(int point, Area[] areas) {
+        int l = 0;
+        int r = areas.length - 1;
+        if (areas[r].to < point) {
+            return r + 1;
+        }
+        if (areas[l].to > point) {
+            return 0;
+        }
+        while (l < r) {
+            int m = (l + r) >> 1;
+            if (areas[m].to > point) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return (areas[l].to > point) ? l : l + 1;
+    }
+
+    static class Area {
+        int from;
+
+        int to;
+
+        Area(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+    }
+}
---
+++ src/test/java/ru/elcoder/stepik/algorithms/quicksort/PointsSolutionTest.java
@@ -0,0 +1,118 @@
+package ru.elcoder.stepik.algorithms.quicksort;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static org.junit.Assert.*;
+import static ru.elcoder.stepik.algorithms.quicksort.PointsSolution.*;
+
+public class PointsSolutionTest {
+
+    @Test
+    public void searchStarted() {
+        final Area[] areas = new Area[5];
+        areas[0] = new Area(1, 2);
+        areas[1] = new Area(3, 4);
+        areas[2] = new Area(5, 6);
+        areas[3] = new Area(7, 8);
+        areas[4] = new Area(9, 10);
+
+        for (int i = 0; i < 12; i++) {
+            assertEquals(PointsSolution.searchStartedNaive(i, areas), PointsSolution.searchStartedQuick(i, areas));
+        }
+    }
+
+    @Test
+    public void searchFinished() {
+        final Area[] areas = new Area[5];
+        areas[0] = new Area(1, 2);
+        areas[1] = new Area(3, 4);
+        areas[2] = new Area(5, 6);
+        areas[3] = new Area(7, 8);
+        areas[4] = new Area(9, 10);
+
+        assertEquals(PointsSolution.searchFinishedNaive(2, areas), PointsSolution.searchFinishedQuick(2, areas));
+        for (int i = 0; i < 12; i++) {
+            System.out.println(i);
+            assertEquals(PointsSolution.searchFinishedNaive(i, areas), PointsSolution.searchFinishedQuick(i, areas));
+        }
+    }
+
+    @Test
+    public void owningAreasCount_1() {
+        final Area[] areasFrom = new Area[1];
+        areasFrom[0] = new Area(3, 7);
+
+        final Area[] areasTo = prepareArrays(areasFrom);
+
+        assertEquals(0, PointsSolution.owningAreasCount(10, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(1, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(5, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(3, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(7, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(8, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(2, areasFrom, areasTo));
+    }
+
+    @Test
+    public void owningAreasCount_2() {
+        final Area[] areasFrom = new Area[2];
+        areasFrom[0] = new Area(3, 7);
+        areasFrom[1] = new Area(9, 10);
+
+        final Area[] areasTo = prepareArrays(areasFrom);
+
+        assertEquals(1, PointsSolution.owningAreasCount(10, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(1, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(5, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(3, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(7, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(8, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(2, areasFrom, areasTo));
+    }
+
+    @Test
+    public void owningAreasCount_2_overlapping() {
+        final Area[] areasFrom = new Area[2];
+        areasFrom[0] = new Area(3, 9);
+        areasFrom[1] = new Area(7, 10);
+
+        final Area[] areasTo = prepareArrays(areasFrom);
+
+        assertEquals(1, PointsSolution.owningAreasCount(10, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(1, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(5, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(3, areasFrom, areasTo));
+        assertEquals(2, PointsSolution.owningAreasCount(7, areasFrom, areasTo));
+        assertEquals(2, PointsSolution.owningAreasCount(8, areasFrom, areasTo));
+        assertEquals(2, PointsSolution.owningAreasCount(9, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(2, areasFrom, areasTo));
+    }
+
+    @Test
+    public void owningAreasCount_2_touching() {
+        final Area[] areasFrom = new Area[2];
+        areasFrom[0] = new Area(3, 8);
+        areasFrom[1] = new Area(8, 10);
+
+        final Area[] areasTo = prepareArrays(areasFrom);
+
+        assertEquals(1, PointsSolution.owningAreasCount(10, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(1, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(5, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(3, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(7, areasFrom, areasTo));
+        assertEquals(2, PointsSolution.owningAreasCount(8, areasFrom, areasTo));
+        assertEquals(1, PointsSolution.owningAreasCount(9, areasFrom, areasTo));
+        assertEquals(0, PointsSolution.owningAreasCount(2, areasFrom, areasTo));
+    }
+
+    private Area[] prepareArrays(Area[] areasFrom) {
+        final Area[] areasTo = Arrays.copyOf(areasFrom, areasFrom.length);
+        Arrays.sort(areasFrom, Comparator.comparingInt(o -> o.from));
+        Arrays.sort(areasTo, Comparator.comparingInt(o -> o.to));
+        return areasTo;
+    }
+}
